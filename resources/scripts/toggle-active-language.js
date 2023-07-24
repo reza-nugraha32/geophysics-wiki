26648400e0f7c2684c00e770eb5c14ca588b4e31
reza-nugraha32 <reza040204@gmail.com>
2023-07-24 15:13:29 +0700

    Changed overall look

--- resources/scripts/toggle-active-language.js
+++ resources/scripts/toggle-active-language.js
@@ -1,9 +1,9 @@
 $(".language-switch").click(function(){
-    if ($(this).children(".language").text() == "EN") {
-        $(this).children(".language").text("ID");
+    if ($(this).children(".language").text() == "English") {
+        $(this).children(".language").text("Indonesia");
     }
     else {
-        $(this).children(".language").text("EN");  
+        $(this).children(".language").text("English");  
     }
     console.log($(this).children(".language").text());
     console.log($("html").attr("lang"))
