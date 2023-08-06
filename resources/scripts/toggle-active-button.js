7aae78dd47124d6af5632386cc8c42f848515456
reza-nugraha32 <reza040204@gmail.com>
2023-08-07 01:50:58 +0700

    Some CSS and JQuery scroll duration adjustment

--- resources/scripts/toggle-active-button.js
+++ resources/scripts/toggle-active-button.js
@@ -69,24 +69,24 @@ $(function toggleActive(){
 
 $(".first-left-scroll").click(function () { 
   var leftPos = $(".first-semester").scrollLeft();
-  $(".first-semester").animate({scrollLeft: leftPos - 400}, 300);
+  $(".first-semester").animate({scrollLeft: leftPos - 300}, 300);
   console.log(leftPos);
 });
 
 $(".first-right-scroll").click(function () { 
   var leftPos = $(".first-semester").scrollLeft();
-  $(".first-semester").animate({scrollLeft: leftPos + 400}, 300);
+  $(".first-semester").animate({scrollLeft: leftPos + 300}, 300);
   console.log(leftPos);
 });
 
 $(".second-left-scroll").click(function () { 
   var leftPos = $(".second-semester").scrollLeft();
-  $(".second-semester").animate({scrollLeft: leftPos - 400}, 300);
+  $(".second-semester").animate({scrollLeft: leftPos - 300}, 300);
   console.log(leftPos);
 });
 
 $(".second-right-scroll").click(function () { 
   var leftPos = $(".second-semester").scrollLeft();
-  $(".second-semester").animate({scrollLeft: leftPos + 400}, 300);
+  $(".second-semester").animate({scrollLeft: leftPos + 300}, 300);
   console.log(leftPos);
 });
