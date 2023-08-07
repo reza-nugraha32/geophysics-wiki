6ac267f33aef0982c4c3c0189c523ff77f61971a
reza-nugraha32 <reza040204@gmail.com>
2023-08-08 04:20:12 +0700

    Update Python code and some CSS layout

--- courses/en/TG2105/codes/exercise_1_1.py
+++ courses/en/TG2105/codes/exercise_1_1.py
@@ -29,4 +29,4 @@ for i in range(n-2,-1,-1):
 
 display('Numerical solutions:', target="exercise_1_1_solution")
 for i in range(n):
-    display("x_%d: %.5f" %(i, solution[i]), target="exercise_1_4_solution")
+    display("x_%d: %.5f" %(i+1, solution[i]), target="exercise_1_4_solution")
--- courses/en/TG2105/codes/exercise_1_2.py
+++ courses/en/TG2105/codes/exercise_1_2.py
@@ -30,4 +30,4 @@ for i in range(n-2,-1,-1):
 
 display('Numerical solutions:', target="exercise_1_2_solution")
 for i in range(n):
-    display("x_%d: %.5f" %(i, solution[i]), target="exercise_1_4_solution")
+    display("x_%d: %.5f" %(i+1, solution[i]), target="exercise_1_4_solution")
