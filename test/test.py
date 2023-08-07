ef9b3d92465016157b4622acb09698a5e87ab746
reza-nugraha32 <reza040204@gmail.com>
2023-08-07 10:29:11 +0700

    Minor CSS adjustment

---
+++ test/test.py
@@ -0,0 +1,33 @@
+def test():
+    import numpy as np
+
+    # Array storing the variables
+    array = np.array([[1, 2, 10], 
+                    [1.1, 2, 10.4]])
+
+    n = len(array)
+
+    # Array storing the solutions
+    solution = np.zeros(n)
+
+    # Forward Elimination Step
+    for i in range(n):
+        for j in range(i+1, n):
+            ratio = array[j][i]/array[i][i]
+            
+            for k in range(n+1):
+                array[j][k] = array[j][k]-ratio*array[i][k]
+
+    # Back Substitution Step
+    solution[n-1] = array[n-1][n]/array[n-1][n-1]
+
+    for i in range(n-2,-1,-1):
+        solution[i] = array[i][n]
+        
+        for j in range(i+1,n):
+            solution[i] = solution[i]-array[i][j]*solution[j]
+        
+        solution[i] = solution[i]/array[i][i]
+
+    print("The augmented upper triangular matrix:\n", array)
+    print("Numerical solutions:\n", solution)
